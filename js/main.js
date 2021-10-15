5223376eff0089e361350df731e345df0e9e8860
Alper Acabey <alperacabey@outlook.com>
2021-10-15 16:36:11 +0300

    0000 initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+(function($) {
+	"use strict";
+
+	$(window).on('load', function() {
+	    $(".preloader").fadeOut("slow", function() {
+	        $(".preloader-left").addClass("slide-left");
+	    });
+	    $("body").css("overflow-y", "auto");
+	});
+})(jQuery);
